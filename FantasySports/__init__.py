172e4d999c00a752eb2036a0b6578c8712295c1f
Taylor Firman <tefirman@gmail.com>
2024-02-06 22:00:20 -0700

    Adding initial version of init file

---
+++ FantasySports/__init__.py
@@ -0,0 +1,10 @@
+"""
+Fantasy Sports.
+
+This repository contains fantasy sports simulators based in Python that were originally 
+used as a training tool for myself, but have since grown into more of a pet project. 
+All applications are meant for entertainment purposes only.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Taylor Firman'
